9dfee2ecf4513eb75311a69c3959dd8d879f1b9c
Chaitanya Kumbhakarna <chaitanyakumbhakarna@gmail.com>
2021-09-14 20:47:58 +0530

    Hotel Rooms Reduction technique done

--- app/controllers/booking_details_controller.rb
+++ app/controllers/booking_details_controller.rb
@@ -1,12 +1,22 @@
 class BookingDetailsController < ApplicationController
 	def create
-		# puts 'create method'
+		
+		#gettting hotel detail to be booked
 		hotel = HotelDetail.get_details(params[:hotel_id])
+
+		#Calculating total price w.r.t rooms and Days
 		total = total_price(hotel.price,params[:rooms],params[:from],params[:to])
 
+		#reducing Hotel rooms from Hotel detail tables
+		hotel.rooms = (hotel.rooms).to_i - (params[:rooms]).to_i
+
 		booking = BookingDetail.new(user_id: params[:user], hotel_detail_id: params[:hotel_id],total_price: total, from_date: params[:from], to_date: params[:to])
-		if booking.save
-			puts 'it is saved'
+		if booking.save && hotel.save
+			flash[:notice] = "Congratulations your Booking was successfully done"
+			redirect_to my_bookings_path
+		else
+			flash[:notice] = "Sorry your Booking was not successfully done TRY AGAIN"
+			redirect_to hotel_detail_path
 		end
 		
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :hotel_details
   devise_for :users
   root 'hotel_details#index'  
-  # get 'my_bookings', to: 'users#my_bookings'
+  get 'my_bookings', to: 'users#my_bookings'
   # get 'booking_details', to: 'users#my_booking'
 end
