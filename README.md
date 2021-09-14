0488f95b5d582e5bca45e97f58cf7619b801307d
Chaitanya Kumbhakarna <chaitanyakumbhakarna@gmail.com>
2021-09-14 19:59:56 +0530

    BookingDetails routess,create functionality done

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ Things you may want to cover:
 
 
     <!-- booking = BookingDetail.new(user_id: current_user, hotel_detail_id: params[:hotel_id], total_price: hotel.price, from_date: params[:from], to_date: params[:to]) -->
+
+<!-- puts hotel.price
+        price = hotel.price.to_i
+        puts params[:rooms]
+        rom = params[:rooms]
+        dif = Date.parse(params[:to]) - Date.parse(params[:from])
+        puts dif.to_i
+        diff = dif.to_i
+        total = (price) * (rom.to_i) * (diff) -->
--- app/controllers/booking_details_controller.rb
+++ app/controllers/booking_details_controller.rb
@@ -1,9 +1,21 @@
 class BookingDetailsController < ApplicationController
 	def create
-		puts 'hie i am here'
+		# puts 'create method'
 		hotel = HotelDetail.get_details(params[:hotel_id])
-		puts hotel.price
-		# booking = BookingDetail.create(user_id: params[:user], hotel_detail_id: params[:hotel_id])
+		total = total_price(hotel.price,params[:rooms],params[:from],params[:to])
+
+		booking = BookingDetail.new(user_id: params[:user], hotel_detail_id: params[:hotel_id],total_price: total, from_date: params[:from], to_date: params[:to])
+		if booking.save
+			puts 'it is saved'
+		end
 		
+	end
+
+	private
+
+	def total_price(price_per_day,no_of_rooms,from_date,to_date)
+		total_days = Date.parse(to_date) - Date.parse(from_date)
+		total_price = (price_per_day) * (no_of_rooms.to_i) * (total_days.to_i)
+		return total_price.to_i
 	end
 end
